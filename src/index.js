d6374a018b65aa316ac3451a631b9d59f62e52b5
commenthol <commenthol@gmail.com>
2017-02-18 21:56:01 +0900

    clones & tests

---
+++ src/index.js
@@ -0,0 +1,173 @@
+/**
+* @copyright 2017 commenthol
+* @license MIT
+*/
+
+module.exports = clones
+
+/**
+* A Deep-Clone of object `source`
+*
+* @static
+* @param {Object} source - clone source
+* @param {Object} [bind] - bind functions to this context
+* @return {Any} deep clone of `source`
+* @example
+* const clones = require('clones')
+*
+* const source = [
+*   {a: {b: 1}},
+*   {c: {d: 2}},
+*   '3',
+*   function () { return 4 }
+* ]
+* // adding circularities
+* source[0].a.e = source[0].a
+*
+* const dest = clones(source)
+* // => [{ a: { b: 1, e: [Circular] } },
+* //     { c: { d: 2 } },
+* //     '3',
+* //     [Function] ]
+*/
+function clones (source, bind) {
+  let opts = {
+    bind: bind,
+    visited: [],
+    cloned: []
+  }
+  return _clone(opts, source)
+}
+
+/**
+* Recursively clone source
+*
+* @static
+* @private
+* @param {Object} opts - options
+* @param {Object} [opts.bind] - optional bind for function clones
+* @param {Array} opts.visited - visited references to detect circularities
+* @param {Array} opts.cloned - visited references of clones to assign circularities
+* @param {Any} source - The object to clone
+* @return {Any} deep clone of `source`
+*/
+function _clone (opts, source) {
+  let target
+  let type = toString.call(source).replace(/^\[object (.*)\]$/, '$1')
+  let isObject = (typeof source === 'object')
+  switch (type) {
+    case 'String':
+    case 'Number':
+    case 'Boolean':
+    case 'Null':
+    case 'Undefined':
+    case 'Symbol':
+    case 'DOMPrototype':  // (browser)
+    case 'process':       // (node) cloning this is not a good idea
+      target = source
+      break
+    case 'Function':
+      target = function () {
+        let _bind = (opts.bind === null ? null : opts.bind || source)
+        return source.apply(_bind, arguments)
+      }
+      target = _props(opts, source, target)
+      break
+    case 'Int8Array':
+    case 'Uint8Array':
+    case 'Uint8ClampedArray':
+    case 'Int16Array':
+    case 'Uint16Array':
+    case 'Int32Array':
+    case 'Uint32Array':
+    case 'Float32Array':
+    case 'Float64Array':
+    case 'Array':
+      target = source.map(function (item) {
+        return _clone(opts, item)
+      })
+      target = _props(opts, source, target)
+      break
+    case 'Date':
+      target = new Date(source)
+      break
+    case 'Error':
+      // EvalError
+      // InternalError
+      // RangeError
+      // ReferenceError
+      // SyntaxError
+      // TypeError
+      // URIError
+      target = new Error(source.message)
+      target = _props(opts, source, target)
+      target.stack = source.stack
+      break
+    case 'RegExp':
+      let flags = source.flags ||
+        (source.global ? 'g' : '') +
+        (source.ignoreCase ? 'i' : '') +
+        (source.multiline ? 'm' : '')
+      target = new RegExp(source.source, flags)
+      break
+    case 'Window': // clone of global objects
+    case 'global':
+    case 'Math':
+    case 'JSON':
+    case 'Console':
+    case 'Navigator':
+    case 'Screen':
+    case 'Object':
+      target = _props(opts, source, target || {})
+      break
+    default:
+      if (/^HTML/.test(type)) {
+        if (source.cloneNode) {
+          target = source.cloneNode(true)
+        } else {
+          target = source
+        }
+      } else if (isObject) {
+        target = _props(opts, source, target || {})
+      } else {
+        // anything else should be a primitive
+        target = source
+      }
+  }
+  return target
+}
+
+/**
+* Clone property while cloning circularities
+*
+* @static
+* @private
+* @param {Object} opts - options
+* @param {Any} source - source object
+* @param {Any} [target] - target object
+* @returns {Any} target
+*/
+function _props (opts, source, target) {
+  let idx = opts.visited.indexOf(source) // check for circularities
+  if (idx === -1) {
+    opts.visited.push(source)
+    opts.cloned.push(target)
+    Object.getOwnPropertyNames(source).forEach(function (key) {
+      var desc
+      if (key === 'prototype') {
+        target[key] = Object.create(source[key])
+      } else if ((desc = Object.getOwnPropertyDescriptor(source, key))) {
+        if (desc.writable) {
+          target[key] = _clone(opts, source[key])
+        } else if (desc.configurable) {
+          Object.defineProperty(target, key, desc)
+        }
+      }
+    })
+    opts.visited.pop()
+    opts.cloned.pop()
+  } else {
+    target = opts.cloned[idx]
+  }
+  return target
+}
---
+++ test/clones.js
@@ -0,0 +1,231 @@
+/* global describe, it */
+
+var assert = require('assert')
+var clone = require('..')
+
+var isBrowser = (typeof window !== 'undefined')
+
+describe('#clone', function () {
+  var tests = [
+    ['Null', null],
+    ['Undefined', undefined],
+    ['String', 'string'],
+    ['Number', 3.14],
+    ['Boolean', true],
+    ['Array', [1, '2', false], true],
+    ['Uint8Array', new Uint8Array([1, 2, 3]), true],
+    ['Object', {a: 1, b: 2}, true],
+    ['Date', new Date('1970-01-01T00:00:00'), true],
+    ['RegExp', new RegExp('test', 'gim'), true]
+  ]
+  tests.forEach(function (test) {
+    var tcase = test[0]
+    var inp = test[1]
+    var isRef = test[2]
+    it('should clone ' + tcase + ' ' + JSON.stringify(inp), function () {
+      var res = clone(inp)
+      assert.deepEqual(res, inp)
+      if (isRef) {
+        assert.ok(res !== inp)
+      }
+    })
+  })
+
+  it('should clone Error', function () {
+    var inp = new Error('boom')
+    var res = clone(inp)
+    assert.ok(res !== inp)
+    assert.equal(res.message, inp.message)
+    assert.equal(res.stack, inp.stack)
+  })
+
+  it('should clone Object of Objects', function () {
+    var inp = {a: {b: {c: 3}}}
+    var res = clone(inp)
+    assert.ok(res !== inp)
+    assert.ok(res.a.b !== inp.a.b)
+    assert.deepEqual(res.a.b, inp.a.b)
+  })
+
+  it('should clone Object of Array of Objects', function () {
+    var inp = {a: [{b: {c: 3}}]}
+    var res = clone(inp)
+    assert.ok(res !== inp)
+    assert.ok(res.a !== inp.a)
+    assert.ok(res.a[0] !== inp.a[0])
+    assert.deepEqual(res, inp)
+  })
+
+  it('should clone Array of Objects', function () {
+    var inp = [{a: 1}, {b: 2}]
+    var res = clone(inp)
+    assert.ok(res !== inp)
+    assert.ok(res[0] !== inp[0])
+    assert.deepEqual(res, inp)
+  })
+
+  it('should clone Array of Objects of Objects', function () {
+    var inp = [{a: {b: 1}}, {c: {d: 2}}, 3, '4']
+    var res = clone(inp)
+    assert.ok(res !== inp)
+    assert.ok(res[0] !== inp[0])
+    assert.ok(res[0].a !== inp[0].a)
+    assert.deepEqual(res, inp)
+  })
+
+  it('should clone function', function () {
+    var inp = function () { return 42 }
+    var res = clone(inp)
+    assert.ok(res !== inp)
+    assert.equal(res(), 42)
+  })
+
+  it('should clone function with props', function () {
+    var inp = function () { return 42 }
+    inp.test = {a: 1, b: 2}
+    var res = clone(inp)
+    assert.ok(res !== inp)
+    assert.equal(res(), 42)
+    assert.ok(res.test !== inp.test)
+    assert.deepEqual(res.test, inp.test)
+  })
+
+  it('should clone Array of functions', function () {
+    var inp = [function () { return 1 }, function () { return 2 }]
+    var res = clone(inp)
+    assert.ok(res !== inp)
+    assert.ok(res[0] !== inp[0])
+    assert.equal(res[0](), inp[0]())
+    assert.equal(res[1](), inp[1]())
+  })
+
+  it('should bind cloned function', function () {
+    var inp = function () { return this.test() }
+    var ctx = {test: function () { return 42 }}
+    var res = clone(inp, ctx)
+    assert.ok(res !== inp)
+    assert.equal(res(), 42)
+  })
+
+  it('should bind cloned function in Object of Array', function () {
+    var inp = {a: [function () { return this.test() }]}
+    var ctx = {test: function () { return 42 }}
+    var res = clone(inp, ctx)
+    assert.ok(res !== inp)
+    assert.equal(res.a[0](), 42)
+  })
+
+  it('should clone a circular object', function () {
+    var inp = {a: {b: 1}}
+    inp.a.c = inp.a
+    // console.log(inp)
+    var res = clone(inp)
+    assert.ok(res !== inp)
+    assert.ok(res.a.c === res.a) // is circular
+  })
+
+  it('should clone everything', function () {
+    var source = {
+      obj: {a: {b: 1}, d: new Date()},
+      arr: [true, 1, {c: 'dee'}],
+      fn: function () { return this.number + 12 }
+    }
+    // adding circularity
+    source.obj.a.e = source.obj.a
+    // do the cloneing (with binding a context)
+    var dest = clone(source, {number: 30})
+
+    // console.log(dest)
+
+    // checks
+    assert.ok(dest !== source)                      // has different reference
+    assert.ok(dest.obj !== source.obj)              // has different reference
+    assert.ok(dest.obj.a !== source.obj.a)          // has different reference
+    assert.ok(dest.obj.d !== source.obj.d)          // has different reference
+    assert.equal(dest.obj.d.toISOString(),
+      source.obj.d.toISOString())                   // has same content
+    assert.ok(dest.obj.a.e !== source.obj.a.e)      // different references for circularities
+    assert.ok(dest.fn !== source.fn)                // has different function reference
+    source.fn = source.fn.bind({number: 29})        // bind `this` for `source`
+    assert.equal(dest.fn(), source.fn() + 1)        // returning the same result
+  })
+
+  describe('should clone the Math object', function () {
+    var inp = Math
+    var res = clone(inp)
+    it('should be different', function () {
+      assert.ok(res !== inp)
+    })
+    it('should give the same result', function () {
+      var same = -4
+      assert.strictEqual(res.cos(same), inp.cos(same))
+    })
+    Object.getOwnPropertyNames(Math).forEach(function (key) {
+      it(key, function () {
+        assert.ok(res[key] !== inp[key])
+      })
+    })
+  })
+
+  describe('should clone the JSON object', function () {
+    var inp = JSON
+    var res = clone(inp)
+    it('should be different', function () {
+      assert.ok(res !== inp)
+    })
+    it('should give the same result', function () {
+      var same = '{"a":1,"b":2,"c":{"d":"three"}}'
+      assert.strictEqual(res.stringify(res.parse(same)), inp.stringify(inp.parse(same)))
+    })
+    Object.getOwnPropertyNames(JSON).forEach(function (key) {
+      it(key, function () {
+        assert.ok(res[key] !== inp[key])
+      })
+    })
+  })
+
+  it('should clone the console object', function () {
+    var inp = console
+    var res = clone(inp, inp)
+    assert.ok(res !== inp)
+    assert.ok(res.log !== inp.log)
+    res.log('works')
+  })
+
+  if (!isBrowser) {
+    it('should clone global object', function () {
+      var inp = global
+      var res = clone(inp, global)
+      assert.ok(res !== inp)
+      assert.ok(res.global !== inp.global) // circular obj is cloned
+    })
+  }
+
+  if (isBrowser) {
+    it('should clone window object', function () {
+      this.timeout(10000) // need this timeout on edge
+      var inp = window
+      var res = clone(inp, window)
+      assert.ok(res !== inp)
+    })
+    it('should clone document object', function () {
+      this.timeout(10000) // need this timeout on edge
+      var inp = document
+      var res = clone(inp)
+      assert.ok(res !== inp)
+      // append an element to body
+      var div = res.createElement('DIV')
+      div.innerHTML = 'works'
+      var body = res.querySelector('body')
+      assert.equal(toString.call(body), '[object HTMLBodyElement]')
+      body.append(div)
+      assert.equal(body.lastChild.innerHTML, 'works')
+      // element should not be part of original DOM
+      body = inp.querySelector('body')
+      assert.equal(body.lastChild.innerHTML, undefined)
+      // overwriting a function does not harm original
+      res.createElement = undefined
+      assert.ok(inp.createElement !== undefined)
+    })
+  }
+})
